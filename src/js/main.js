253b1a5b6af2e7f9019c050a1e1d3154fc3e009f
Yerzhan <Hancogk3333@gmail.com>
2020-08-04 01:07:59 +0400

    Сделано вертска под мобилку

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,24 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close')
+// $('.menu-btn').on('click', function(e){
+//   e.preventDefault;
+//   $(this).toggleClass('menu-btn_active');
+//   $('.menu-nav').toggleClass('menu-nav__active');
+// });
 
-button.addEventListener('click', function() {
-   modal.classList.add('modal_active');
-});
 
-close.addEventListener('click', function() {
-  modal.classList.remove('modal_active');
-});
+
+
+
+
+// var button = document.querySelector('#button');
+// var modal = document.querySelector('#modal');
+// var close = document.querySelector('#close')
+
+// button.addEventListener('click', function() {
+//    modal.classList.add('modal_active');
+// });
+
+// close.addEventListener('click', function() {
+//   modal.classList.remove('modal_active');
+// });
+
+// alert('script')
